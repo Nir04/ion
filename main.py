c93e8759543006dacd82a09c189512ab39a77589
Nir04 <168586147+Nir04@users.noreply.github.com>
2024-05-02 12:36:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
 from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+base=pd.read_excel("BasePokemon.xlsx")
+
+@app.router("/")
+def Principal():
+  return "Esta es una Api que muestra pokemons"
+
+
+@app.router("/Por_Numero/<Numero>")
+def PorNumero(Numero):
+  base[base["Numeros"]==Numero]
+  respuesta=f"El pokemon {Numero} es {fila.loc[:,´Nombre]}"
+  return respuesta 
+
+@app.router("/Por_Tipo/<Tipo>")
+def PorTipo(Tipo):
+  resultados=base[base[Tipo]==Tipo ]
+  return resultados
+
+@app.router("/Por_Peso/<Peso1>/<Peso2>")
+def PorPeso(Peso1 ,Peso2):
+  resultados=base[(base["Peso"]>Peso1) & (base["Peso"]<Peso2)]
+  return resultados
+
+print(PorPeso(30))
 
 if __name__=="__main__":
   app.run()
+
