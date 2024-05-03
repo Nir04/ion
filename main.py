ed39c2db50e169be36a654747ced1cf7ed31bffb
Nir04 <168586147+Nir04@users.noreply.github.com>
2024-05-03 13:56:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,33 @@ from flask import Flask
 
 app=Flask(__name__)
 
-base=pd.read_excel("BasePokemon.xlsx")
+base=pd.read_excel("nacimiento.xlsx")
 
 @app.router("/")
 def Principal():
-  return "Esta es una Api que muestra pokemons"
+  return "Esta es una Api que muestra informacion personal"
 
 
-@app.router("/Por_Numero/<Numero>")
-def PorNumero(Numero):
-  base[base["Numeros"]==Numero]
-  respuesta=f"El pokemon {Numero} es {fila.loc[:,´Nombre]}"
-  return respuesta 
+@app.router("/Por_Nombre/<Nombre>")
+def PorNombre(Nombre):
+  base[base["Nombre"]==Nombre]
+  respuesta=f"Se llama {Nombre} es {fila.loc[:,´Nombre]}"
+  return respuesta
 
-@app.router("/Por_Tipo/<Tipo>")
-def PorTipo(Tipo):
-  resultados=base[base[Tipo]==Tipo ]
+@app.router("/Por_Edad/<Edad>")
+def PorEdad(Edad):
+  resultados=base[base[Edad]==Edad ]
   resultado=str(resultados)
   return resultados
 
-@app.router("/Por_Peso/<Peso1>/<Peso2>")
-def PorPeso(Peso1 ,Peso2):
-  resultados=base[(base["Peso"]>Peso1) & (base["Peso"]<Peso2)]
-  resultados=str(resultados)
+
+@app.router("/Por_Genero/<Edad>")
+def PorGenero(Genero):
+  resultados=base[base[Genero]==Genero ]
+  resultado=str(resultados)
   return resultados
 
-print(PorPeso(30))
 
 if __name__=="__main__":
   app.run()
-
-
 
